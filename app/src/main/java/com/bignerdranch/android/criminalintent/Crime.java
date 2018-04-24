91c8a1d80254cd00a99c8b651f59e0ee87f2b7a3
vibhavk <vibhavkunj@hotmail.com>
2018-04-24 22:26:04 +0530

    First Commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by vibhav on 24/4/18.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
